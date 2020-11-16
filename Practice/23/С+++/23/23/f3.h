89088c1c6606728e515a651bae16de431d58b14b
sedyukooo <sedyukooo@mailru>
2020-11-16 12:28:40 +0300

    2 пара 16.11

--- Practice/23/С+++/23/23/f3.h
+++ Practice/23/С+++/23/23/f3.h
@@ -2,8 +2,13 @@
 #include "f1.h"
 #include <iostream>
 
-int main() {
-	
+int sin()
+{
+    double x, y, rezult = 0;
+    int i = 0;
+    for (i; i < y; ++i)
+        rezult = rezult + (pow(-1, i) * (pow(x, 2 * i + 1)) / (factorial(2 * i + 1)));
+   
 }
 #endif F3_H
 
---
+++ Practice/24/C++/24/24/24.cpp
@@ -0,0 +1,9 @@
+﻿#include <json.hpp>
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+	ifstream file("")
+
+}
